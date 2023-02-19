63283112a521b9123fdb35862025f02273d1947b
LisReyes <lisset.rey2212@gmail.com>
2023-02-18 21:35:53 -0600

    Modificando el parametor de obtencion de hora

--- reto3.js
+++ reto3.js
@@ -4,16 +4,16 @@
 // new Date().getDay(). da como resultado el dia de la semana del 0-6 siendo 0-Domingo y asi sucesivamente
 
 const businessHours = () => {
-   //  data
-   let today = new Date();
-   let day = today.getDay(); 
-   // hour
-   let hour = today.toLocaleTimeString();
-   // let's start with the determinate if it is a WorkDay
-   if ((day>0 && day<6) && (hour>9 && hour<18)){
-      console.log(" Buh is a workin day");
-   }else{
-      console.log(" Yeah is time to rest ");
-   }
+    //  data
+    let today = new Date();
+    let day = today.getDay();
+    // hour
+    let hour = today.toLocaleTimeString();
+    // let's start with the determinate if it is a WorkDay
+    if ((day > 0 && day < 6) && (hour > 9 && hour < 18)) {
+        console.log(" Buh is a working day");
+    } else {
+        console.log(" Yeah is time to rest ");
+    }
 }
 businessHours();
